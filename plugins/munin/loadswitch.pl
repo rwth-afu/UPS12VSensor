2d8e0cdf2f9a5cca9f062a44445ea6a7a73ef720
dh3wr <ralfwil@gmx.de>
2016-07-04 16:12:33 +0200

    Munin Plugin RC1

--- plugins/munin/loadswitch.pl
+++ plugins/munin/loadswitch.pl
@@ -3,7 +3,7 @@
 use IO::Socket::INET;
 
 my $server_host = "localhost";
-my $server_port = "1234";
+my $server_port = "1337";
 
 if ($ARGV[0] and $ARGV[0] eq "config") {
 	print "graph_title Voltage and Current on 12 V UPS\n";
@@ -11,13 +11,13 @@ if ($ARGV[0] and $ARGV[0] eq "config") {
 	print "graph_vlabel Value in Volt or Ampere\n";
 	print "graph_category ups\n";
 	print "graph_info This graph shows the voltage and current on the 12 V UPS board.\n";
-	print "UBat.label Voltage Bat\n";
+	print "UBat.label Voltage Bat in Volts\n";
 	print "UBat.type GAUGE\n";
-	print "UNT.label Voltage Power Supply\n";
+	print "UNT.label Voltage Power Supply in Volts\n";
 	print "UNT.type GAUGE\n";
-	print "IBat.label Current Bat\n\n";
+	print "IBat.label Current Bat in Amperes\n";
 	print "IBat.type GAUGE\n";
-	print "INT.label Current Power Supply\n";
+	print "INT.label Current Power Supply in Amperes\n";
 	print "INT.type GAUGE\n";
 } else {
 
@@ -25,7 +25,7 @@ if ($ARGV[0] and $ARGV[0] eq "config") {
 	# flush after every write
 	$| = 1;
 
-	my ($socket);
+	my $socket;
 
 	# creating object interface of IO::Socket::INET modules which internally creates 
 	# socket, binds and connects to the TCP server running on the specific port.
@@ -35,16 +35,24 @@ if ($ARGV[0] and $ARGV[0] eq "config") {
 	Proto => 'tcp',
 	) or die "ERROR in Socket Creation : $!\n";
 
-	print "TCP Connection Success.\n";
+#	print "TCP Connection Success.\n";
 
 	# read the socket data sent by server.
 	$data = <$socket>;
 
-	print "Received from Server : $data\n";
+#	print "Received from Server : $data\n";
+	
+	# Extract data separated by ";"
+	my ($ubat,$ibat,$unt,$int) = split /;/, $data;
 
 	# Give out on STDOUT in format
 	#  UBat.value 12.234
 	#  IBat.value 1.234
 	#  UNT.value 12.234
 	#  INT.value 1.234
+
+	print "UBAT.value $ubat\n";
+	print "IBAT.value $ibat\n";
+	print "UNT.value $unt\n";
+	print "INT.value $int\n";
 }
