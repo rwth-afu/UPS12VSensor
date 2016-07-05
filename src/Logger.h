cfc4191dd3649cdde0353eace107492e6222770e
Philipp Thiel <pthiel@radicalworlds.de>
2016-07-05 09:22:33 +0200

    Made Logger class non-copyable.

--- src/Logger.h
+++ src/Logger.h
@@ -10,9 +10,13 @@ class Logger
 public:
 	Logger(LogLevel level);
 
+	Logger(const Logger& o) = delete;
+
 	Logger(LogLevel level, const std::string& filename);
 
 	~Logger() = default;
+
+	Logger& operator=(const Logger& o) = delete;
 
 	void write(LogLevel level, const std::string& msg);
 
